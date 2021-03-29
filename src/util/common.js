600b5203af2f453aa9d30f3aa16e3869f712bb0e
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-29 23:30:53 +0500

    fix: ios example

--- src/util/common.js
+++ src/util/common.js
@@ -1,13 +1,17 @@
 const fs = require('fs-extra');
 const path = require('path');
+const isExample = process.env.IS_EXAMPLE === 'TRUE';
 const DEFAULT_FILE_NAME = 'jnikeys.json';
-const PROJECT_ROOT_DIR_PATH = path.join(__dirname, '../../../../');
+const PROJECT_ROOT_DIR_PATH = path.join(
+  __dirname,
+  isExample ? '../../example/' : '../../../../'
+);
 const PACKAGE_ROOT_DIR_PATH = path.join(__dirname, '../../');
+const JNI_IOS_PATH = path.join('node_modules', 'react-native-keys', 'ios');
+const JNI_IOS_EXAMPLE_PATH = path.join('../', 'ios');
 const IOS_DIR_PATH = path.join(
   PROJECT_ROOT_DIR_PATH,
-  'node_modules',
-  'react-native-keys',
-  'ios'
+  isExample ? JNI_IOS_EXAMPLE_PATH : JNI_IOS_PATH
 );
 const ANDROID_DIR_PATH = path.join(
   PROJECT_ROOT_DIR_PATH,
@@ -30,6 +34,7 @@ const ANDROID_JNI_DIR_PATH = path.join(
   'com',
   'reactnativejnikeys'
 );
+
 console.log(ANDROID_JNI_DIR_PATH, 'ANDROID_JNI_DIR_PATHANDROID_JNI_DIR_PATH');
 module.exports.getJniKeys = (JNI_FILE_NAME) => {
   const jniJsonFilePath = `${PROJECT_ROOT_DIR_PATH}${JNI_FILE_NAME}`;
@@ -41,6 +46,7 @@ module.exports.getJniKeys = (JNI_FILE_NAME) => {
 module.exports.makeFileInIosDir = (fileContent, fileName) => {
   try {
     const iosCppFilePath = path.join(IOS_DIR_PATH, fileName);
+    console.log(iosCppFilePath, 'iosCppFilePatheeee');
     fs.outputFileSync(iosCppFilePath, fileContent);
     return true;
   } catch (error) {
