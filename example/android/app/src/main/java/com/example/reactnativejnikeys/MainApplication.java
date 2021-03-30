f63d36f3a16439a32fa8d126e14f2df9d0edbb3e
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-30 23:53:15 +0500

    fix: native linking of android example

--- example/android/app/src/main/java/com/example/reactnativejnikeys/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativejnikeys/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+import com.reactnativejnikeys.JniKeysPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for JniKeysExample:
             // packages.add(new MyReactNativePackage());
+             packages.add(new JniKeysPackage());
           return packages;
         }
 
