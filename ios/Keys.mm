b4c29ee9f0503b52eeca420ec790e00994844d6c
Shahzaib Ali Sherazi <sherazi@theentertainerasia.com>
2022-11-08 18:43:03 +0500

    Fix running on Xcode 14.1
    - Remove Format string is not a string literal (potentially insecure)

--- ios/Keys.mm
+++ ios/Keys.mm
@@ -10,7 +10,7 @@
   + (NSString *)secureFor: (NSString *)key {
       @try {
           NSString* stringfyData = [NSString stringWithCString:Crypto().getJniJsonStringyfyData(privateKey).c_str() encoding:[NSString defaultCStringEncoding]];
-          NSLog(stringfyData);
+          NSLog(@"%@", stringfyData);
           NSData *data = [stringfyData dataUsingEncoding:NSUTF8StringEncoding];
           NSMutableDictionary *s = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
           NSString *value =[s objectForKey:key];
