bfff2e3992891bfd7021437856a9ad6f8edd57a4
Douglas Galdino <dougg0k@gmail.com>
2025-07-18 16:47:12 -0300

    fixed react import

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RNKeys from 'react-native-keys';
 
--- exampleExpo/src/App.tsx
+++ exampleExpo/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RNKeys from 'react-native-keys';
 export default function App() {
